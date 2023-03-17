c167680a12e3ba3402613cbdb3465e570123eec9
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2023-03-17 23:13:29 +0530

    vercel

--- backend/main.py
+++ backend/main.py
@@ -22,4 +22,4 @@ async def root(url):
         return {"error": "error"}
     
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=3000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
