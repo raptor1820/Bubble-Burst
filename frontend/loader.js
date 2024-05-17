f20eb38930d7c8c284f67035d3998ceec370a89c
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2024-05-17 13:01:11 +0530

    updated URL for request

--- frontend/loader.js
+++ frontend/loader.js
@@ -1,33 +1,34 @@
 function driver() {
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    const currurl = tabs[0].url;
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        const currurl = tabs[0].url;
 
-    load(currurl);
-  });
+        load(currurl);
+    });
 }
 
 async function getUrls(url) {
-  const response = await fetch(
-    "https://resonant-tube-372613.el.r.appspot.com/?url=" + url
-  );
+    const response = await fetch(
+        "https://apibubbleburst.ritwic.com/?url=" + url
+    );
 
-  return response.json();
+    return response.json();
 }
 async function load(url) {
-  const dispArr = await getUrls(url);
+    const dispArr = await getUrls(url);
 
-  for (let i = 0; i < 10; i++) {
-    print(dispArr[i]);
-    if (!dispArr[i]) {
-      console.log("breaking");
+    for (let i = 0; i < 10; i++) {
+        print(dispArr[i]);
+        if (!dispArr[i]) {
+            console.log("breaking");
+        }
+        document.getElementById("title" + i).innerHTML = dispArr[i].title;
+        document.getElementById("title" + i).href = dispArr[i].link;
+        document.getElementById("origin" + i).innerHTML =
+            dispArr[i].displayLink;
+        // document.getElementById("readtime" + i).innerHTML = dispArr[i].read_time;
     }
-    document.getElementById("title" + i).innerHTML = dispArr[i].title;
-    document.getElementById("title" + i).href = dispArr[i].link;
-    document.getElementById("origin" + i).innerHTML = dispArr[i].displayLink;
-    // document.getElementById("readtime" + i).innerHTML = dispArr[i].read_time;
-  }
-  document.getElementById("loading").style.opacity = "0.0";
-  document.getElementById("outer").style.opacity = "1.0";
-  document.getElementById("buttons").style.opacity = "1.0";
+    document.getElementById("loading").style.opacity = "0.0";
+    document.getElementById("outer").style.opacity = "1.0";
+    document.getElementById("buttons").style.opacity = "1.0";
 }
 document.addEventListener("DOMContentLoaded", driver);
